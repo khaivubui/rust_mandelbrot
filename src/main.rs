f26282db0abfae2d23b81fe9192bc4be2860dbf1
Khai Bui <khaibui@Khais-MacBook-Pro.local>
2018-03-18 22:45:19 -0700

    Write single thread solution

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,13 @@
 extern crate num;
 use num::Complex;
+
+extern crate image;
+use image::ColorType;
+use image::png::PNGEncoder;
+
+use std::fs::File;
 use std::str::FromStr;
+use std::io::Write;
 
 fn parse_pair<T: FromStr>(s: &str, seperator: char) -> Option<(T, T)>{
     match s.find(seperator) {
@@ -80,7 +87,6 @@ fn render(pixels: &mut[u8],
     }
 }
 
-#[allow(dead_code)]
 fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     let mut z = Complex{re: 0.0, im: 0.0};
     for i in 0..limit {
@@ -93,6 +99,34 @@ fn escape_time(c: Complex<f64>, limit: u32) -> Option<u32> {
     None
 }
 
+fn write_image(filename: &str,
+               pixels: &[u8],
+               bounds: (usize, usize))
+               -> Result<(), std::io::Error> {
+    let output = File::create(filename)?;
+    let encoder = PNGEncoder::new(output);
+    encoder.encode(&pixels, bounds.0 as u32, bounds.1 as u32, ColorType::Gray(8))?;
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() != 5 {
+        writeln!(std::io::stderr(),
+                 "Usage mandelbrot FILE PIXELS UPPERLEFT LOWERRIGHT")
+                 .unwrap();
+        writeln!(std::io::stderr(),
+                 "Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                 args[0])
+                 .unwrap();
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x').expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3]).expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4]).expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+    render(&mut pixels, bounds, upper_left, lower_right);
+    write_image(&args[1], &pixels, bounds).expect("error writing PNG file");
 }
